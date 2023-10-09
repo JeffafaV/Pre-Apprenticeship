b3f9e57fe0c824b21ce3b61b2d1af0296d53dc81
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2023-10-08 21:30:05 -0700

    Adding Grading Program

---
+++ Java Programs/Grading/Grading.java
@@ -0,0 +1,47 @@
+public class Grading {
+    private double quiz1;
+    private double quiz2;
+    private double quiz3;
+    private double midterm;
+    private double finalExam;
+    private double numericGrade;
+    private char letterGrade;
+    
+    public void setQuiz1(int q1) {
+        quiz1 = q1;
+    }
+    
+    public void setQuiz2(int q2) {
+        quiz2 = q2;
+    }
+    
+    public void setQuiz3(int q3) {
+        quiz3 = q3;
+    }
+    
+    public void setMidterm(int m) {
+        midterm = m;
+    }
+    
+    public void setFinalExam(int f) {
+        finalExam = f;
+    }
+    
+    public void evalNumGrade() {
+        
+    }
+    
+    public void evalLetGrade() {
+        
+    }
+    
+    public double getNumeric() {
+        
+    }
+    
+    public double getLetter() {
+        
+    }
+    
+    public 
+}

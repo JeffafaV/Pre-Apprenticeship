39e753a68bd1c7291e14515ca724e036ab8b5d01
Jeff Victorino <47927752+JeffafaV@users.noreply.github.com>
2023-10-12 10:49:22 -0700

    Create sakilaSelect.sql

---
+++ SQL_Scripts_for_MySQL/sakilaSelect.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sakila.actor WHERE first_name = "Cuba";
+SELECT title,film_id FROM sakila.film WHERE film_id > 5 AND film_id < 20;
+SELECT first_name, last_name, email FROM sakila.customer WHERE last_name = "Rodriguez" OR last_name LIKE "Mc%";
+SELECT * FROM sakila.film WHERE title LIKE 'a%';
+SELECT title from sakila.film WHERE language_id NOT IN(SELECT language_id from sakila.language WHERE name = "English" OR name = "Japanese");
+SELECT rental_id, payment_date FROM sakila.payment WHERE amount > .99;
+SELECT staff_id, customer_id FROM sakila.payment WHERE amount > .99;

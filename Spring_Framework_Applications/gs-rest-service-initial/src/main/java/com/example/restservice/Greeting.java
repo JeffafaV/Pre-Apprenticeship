c2de1c49fe15087641dfe5523c2bff97d03b1d72
Jeff Victorino <47927752+JeffafaV@users.noreply.github.com>
2023-10-27 09:38:24 -0700

    Spring Rest Service Program

---
+++ Spring_Framework_Applications/gs-rest-service-initial/src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,23 @@
+package com.example.restservice;
+
+public class Greeting {
+	// each Greeting has content and an ID
+    private final long id;
+    private final String content;
+
+    // public constructor
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    // getter for ID
+    public long getId() {
+        return id;
+    }
+
+    // getter for content
+    public String getContent() {
+        return content;
+    }
+}

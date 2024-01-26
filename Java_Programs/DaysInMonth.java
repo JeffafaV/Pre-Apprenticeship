f113c4b854dc03c1e8cd5c958fddbdc4be4b5ec9
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2024-01-25 17:47:04 -0800

    Update DaysInMonth.java

--- Java_Programs/DaysInMonth.java
+++ Java_Programs/DaysInMonth.java
@@ -12,34 +12,44 @@ public class DaysInMonth {
 		// creating a scanner object to allow user input
 		Scanner sc = new Scanner(System.in);
 		
-		// boolean used as a condition in the do while loop
+		// boolean used for condition in the do while loop
 		boolean b = true;
 		
 		// a do while loop will go through the body at least once
 		do
 		{
+			// displaying prompt and taking user input for year
 			System.out.print("Enter year: ");
 			int year = sc.nextInt();
+
+			// displaying prompt and taking user input for month
 			System.out.print("Enter month: ");
 			int month = sc.nextInt();
-			/*
-			System.out.print("Enter day: ");
-			int day = sc.nextInt();
-			*/
+			
+			// try statement runs code in body and checks for any errors
 			try
 			{
-				Year y = Year.of(year);
-				YearMonth ym = y.atMonth(month);
+				// creating Year object using the year the user inputted
+				Year y = Year.of(year); // can cause error if year is invalid
+				// creating YearMonth object using the month the user inputted
+				YearMonth ym = y.atMonth(month); // can cause error if month is invalid
+
+				// Note that the next two lines of code won't run if error occured
+				// displays prompt for the number of days in a certain month
 				System.out.println("The number of days in " + ym.getMonth() + " of " + 
 				ym.getYear() + " is " + ym.lengthOfMonth());
-				/*System.out.println("The number of days in " + ld + " is " + daysInMonth);*/
+				// sets b to false
 				b = false;
 			}
+			// catch statement runs code in body if error/exception was found in try block
 			catch (Exception e)
 			{
+				// displays prompt for bad input
 				System.out.println("Invalid input. Try again.");
 			}
-		} while (b == true); // continues looping until the user enters correct input
+		}
+		// continues looping until b is false which is when the user enters correct input
+		while (b == true);
 		
 		// terminates scanner
 		sc.close();
